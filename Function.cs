c3f26bf11c7e5d0224576937ac2227839f13ae74
LlubomirKiosev <kiosev.lubomir@abv.bg>
2020-05-26 03:11:21 +0300

    Initial

---
+++ Function.cs
@@ -0,0 +1,136 @@
+﻿using Syncfusion.Drawing;
+using Syncfusion.XlsIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckCalls1._1
+{
+    public class Function
+    {
+        public static async Task Main(string[] args)
+        {
+
+            var line = await GetValueFromTxtDocunment(args[0]);
+
+            int col = await GetColumn(args[1].ToString());
+
+            FileStream inputStream = new FileStream(args[1], FileMode.Open, FileAccess.ReadWrite);
+
+            using (ExcelEngine SaveExcelEngine = new ExcelEngine())
+            {
+                IWorkbook workbook = SaveExcelEngine.Excel.Workbooks.Open(inputStream);
+
+                int firstRow = 6;
+
+                var sheet = workbook.ActiveSheet;
+
+                while (sheet.GetValueRowCol(firstRow, 1).ToString() != string.Empty)
+                {
+                    var targetData = sheet.GetValueRowCol(firstRow, 1).ToString();
+
+                    if (line.Contains(targetData) && sheet.GetValueRowCol(firstRow, col).ToString() == string.Empty)
+                    {
+                        sheet.Range[firstRow, col].CellStyle.Color = Color.White;
+                        sheet.SetValueRowCol("T", firstRow, col);
+                        workbook.Version = ExcelVersion.Excel2016;
+                        workbook.SaveAs(inputStream);
+                    }
+                    else if (sheet.GetValueRowCol(firstRow, col).ToString() == string.Empty)
+                    {
+                        sheet.Range[firstRow, col].CellStyle.Color = Color.Yellow;
+                        sheet.Range[firstRow, col].BorderAround(ExcelLineStyle.Thick, Color.Black);
+                        workbook.Version = ExcelVersion.Excel2016;
+                        workbook.SaveAs(inputStream);
+                    }
+
+                    firstRow++;
+                }
+                inputStream.Dispose();
+            }
+        }
+
+        private static async Task<int> GetColumn(string filePath)
+        {
+            int col = 0;
+            await Task.Run(() =>
+            {
+                if (DateTime.Now.Hour >= 0 && DateTime.Now.Hour < 3)
+                {
+                    if (filePath.Contains("Неделя") || filePath.Contains("Събота"))
+                        col++;
+                    col += 7;
+                }
+                else if (DateTime.Now.Hour >= 3 && DateTime.Now.Hour < 5)
+                {
+                    if (filePath.Contains("Неделя") || filePath.Contains("Събота"))
+                        col++;
+                    col += 8;
+                }
+                else if (DateTime.Now.Hour >= 5 && DateTime.Now.Hour < 7)
+                {
+                    if (filePath.Contains("Неделя") || filePath.Contains("Събота"))
+                        col++;
+                    col += 9;
+                }
+                else if (DateTime.Now.Hour >= 7 && DateTime.Now.Hour < 11)
+                {
+                    col = 4;
+                }
+                else if (DateTime.Now.Hour >= 11 && DateTime.Now.Hour < 17)
+                {
+                    col += 5;
+                }
+                else if (DateTime.Now.Hour >= 17 && DateTime.Now.Hour < 19)
+                {
+                    if (filePath.Contains("Неделя") || filePath.Contains("Събота"))
+                        col++;
+                    col += 5;
+                }
+                else
+                {
+                    if (filePath.Contains("Неделя") || filePath.Contains("Събота"))
+                        col++;
+                    col += 6;
+                }
+            });
+            return col;
+        }
+
+        private static async Task<List<string>> GetValueFromTxtDocunment(string filePath)
+        {
+            var line = new List<string>();
+            var replaceElements = new List<string>();
+
+            await Task.Run(() =>
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string[] separatingStrings = { " ", ",", ".", ":", ";", ", ", "\n", "\r" };
+
+                    line = sr.ReadToEnd().Split(separatingStrings, StringSplitOptions.RemoveEmptyEntries).ToList();
+                }
+
+                for (int i = 0; i < line.Count; i++)
+                {
+                    var currentValue = line[i];
+
+                    if (currentValue.Contains("0") && currentValue.Length > 4)
+                    {
+                        var newElement = new StringBuilder(line[i]);
+
+                        var lastIndexWithZero = currentValue.LastIndexOf('0');
+                        newElement[lastIndexWithZero] = '-';
+                        replaceElements.Add(newElement.ToString());
+                    }
+                }
+
+            });
+            line.AddRange(replaceElements);
+            return line;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckCalls1._1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        private void btnOpenTxtFiles_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                openFileDialog = new OpenFileDialog();
+                openFileDialog.Multiselect = false;
+                openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                if (openFileDialog.ShowDialog() == true)
+                    txtFile.Text = openFileDialog.FileName;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error!");
+            }
+
+        }
+
+        private void btnOpenXlxsFiles_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                openFileDialog = new OpenFileDialog();
+                openFileDialog.Multiselect = false;
+                openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                if (openFileDialog.ShowDialog() == true)
+                    xlxsFile.Text = openFileDialog.FileName;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error!");
+            }
+
+        }
+
+        private async void btnSubmitFiles_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                string[] paths = new string[2] { txtFile.Text, xlxsFile.Text, };
+                await Function.Main(paths);
+
+                MessageBoxResult result = MessageBox.Show($"Time: {stopWatch.Elapsed.ToString(@"mm\:ss\.ff")}min. Ready. Do you want to close this window?",
+                                          "Confirmation",
+                                          MessageBoxButton.YesNo,
+                                          MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    Application.Current.Shutdown();
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error!");
+            }
+        }
+    }
+}
