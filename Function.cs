4ebdd3891c444eb132d14df33f1d18ea630127db
LlubomirKiosev <kiosev.lubomir@abv.bg>
2020-09-24 00:49:19 +0300

    Remove using

--- Function.cs
+++ Function.cs
@@ -4,12 +4,10 @@
     using Syncfusion.XlsIO;
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.IO;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
-    using System.Windows;
 
     public class Function
     {
